eac6b0b5f58d657425d6b7ca5454fca4f5081462
Morgan B <dragonmorgan@icloud.com>
2022-06-02 16:17:30 -0700

    super serial

--- Assets/Swimmer.cs
+++ Assets/Swimmer.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Swimmer : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 5f;
+    [SerializeField] float moveSpeed = 0.1f;
+
     void Start()
     {
     }
 
     void Update()
     {
-        transform.Rotate(0, 0, 5);
-        transform.Translate(0, 0.1f, 0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
